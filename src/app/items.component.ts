a0c28a4d1086d0f370ddffaed985d1396ca87a7a
Aroxyson <aroxyson@gmail.com>
2018-06-27 21:46:05 +0500

    Отображение флагов в виде картинок

--- src/app/items.component.ts
+++ src/app/items.component.ts
@@ -1,10 +1,13 @@
 import { Component, Input } from "@angular/core";
 
-
 @Component ({
     selector: "items",
     template: `<div class="bg-success col-12 mb-1 mt-1 border">
                 <span>{{ item.name }}</span>
+                <img *ngIf="item.sun" alt="Sun" src="{{item.sun}}">
+                <img *ngIf="item.heart" alt="heart" src="{{item.heart}}">
+                <img *ngIf="item.flash" alt="flash" src="{{item.flash}}">
+                <img *ngIf="item.flower" alt="flower" src="{{item.flower}}">
                </div>`
 })
 
@@ -12,5 +15,5 @@ export class ItemsComponent {
 
     @Input() item: ItemsComponent;
 
-    
+
 }
--- src/app/items.ts
+++ src/app/items.ts
@@ -1,8 +1,25 @@
 export class Items {
     name: string = '';
     flags: string = '';
-  
+    sun: string = '';
+    heart: string = '';
+    flash: string = '';
+    flower: string = '';
+
     constructor(values: Object = {}) {
-      Object.assign(this, values);
+        Object.assign(this, values);
+
+        if (this.flags.join().search("sun") != -1) {
+          this.sun = require("../images/sun.png");
+        }
+        if (this.flags.join().search("heart") != -1) {
+            this.heart = require("../images/heart.png");
+        }
+        if (this.flags.join().search("flash") != -1) {
+            this.flash = require("../images/flash.png");
+        }
+        if (this.flags.join().search("flower") != -1) {
+            this.flower = require("../images/flower.png");
+        }
     }
 }
